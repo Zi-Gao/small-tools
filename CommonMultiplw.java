5d308539c1278d43e320543a19de85333f9132db
Zi-Gao <gaogaohahaa@gmail.com>
2020-06-23 21:20:45 +0800

    改版最小公倍数计算

--- CommonMultiplw.java
+++ CommonMultiplw.java
@@ -1,34 +1,45 @@
 import java.util.Scanner;
-
-public class CommonMultiplw {//获取最小公倍数
-	
-	static public void Core(long a,long b) {//主方法
-		long c;
-		if(a>b) {//因公倍数不会小于最小的数所以从最小的数开始试
-			c = a;
-		}
-		c = b;
-		for(;true;c++) {
-			if(c % a == 0 && c % b ==0) {//如果满足条件执行语句
-				System.out.println(a+"和"+b+"的最小公倍数是："+c);//输出答案
-				break;//停止循环
+public class CommonMultiplw{
+	static public void Core(final String[] arr) {//核心
+		long c = 1;
+		Loop:for(;true;c++) {//暴力破解
+			if (c == Long.MAX_VALUE){//防溢出
+				System.out.println("此程序记录最小公倍数值为Long,现在为9223372036854775807,继续将会溢出,已自动停止");//打印
+				break;//停止
+			}
+			for(String bStr:arr) {//数组测试
+				// System.out.println(b);
+				if(bStr == null){//若为null表示已经结束,输入答案
+					System.out.println("最小公倍数为:" + c);//打印
+					break Loop;//停止
+				}else if(c % Long.valueOf(bStr).intValue() != 0 ){//如果不对就试下一个数
+					continue Loop;//结束本次循环
+				}
 			}
 		}
 	}
-	
-	public static void main(String[] args) {
-		Long a,b;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("请输入第一个因数");
-		String str1 = sc.nextLine();
-		System.out.println("请输入第二个因数");
-		String str2 = sc.nextLine();
-		if (Lnput.CoreLong(str1) == 0 && Lnput.CoreLong(str2) == 0){//检测是否超过Long值
-			a = Long.valueOf(str1).longValue();
-			b = Long.valueOf(str2).longValue();
-			CommonMultiplw.Core(a, b);
-		}else if(Lnput.CoreLong(str1) == 1 | Lnput.CoreLong(str2) == 1){//若不是则输出
-			System.out.println("请输入Long值");
+	public static void main(String[] args) {//主方法
+		Scanner sc = new Scanner(System.in);//new控制台输入
+		int i = 0;//记录输入次数
+		String[] arr = new String[320339960];
+		for (;true;){//重复输入
+				if (i == Integer.MAX_VALUE){//防溢出
+					System.out.println("此程序记录因数个数值为Int,2147483647,继续将会溢出,已自动停止");//输出
+					break;//停止
+			}
+			System.out.println("请输入因数.输入0为开始计算");//打印标题引导用户输入
+			String str = sc.nextLine();//输入方法
+			if(Lnput.CoreLong(str) == 1 | str == null){//防溢出,防止直接输入null
+				System.out.println("请输入数字,并为Long类型,请重新输入");//打印并跳过本次循环
+				continue;
+			}
+			if(Long.valueOf(str).longValue() == 0){//调用核心运算
+				CommonMultiplw.Core(arr);//调用核心
+				break;
+			}if(Long.valueOf(str).longValue() != 0){//自增与存入数组
+				arr[i] = str;//存入
+				i++;//自增
+			}
 		}
 	}
 }
