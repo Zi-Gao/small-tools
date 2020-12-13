59ae5ef47cfd5717f2c4cca0a267ccce0274b434
Your Name <gaogaohahaa@gmail.com>
2020-12-13 20:27:15 +0800

    Core与交互分离

--- binaryConversion.java
+++ binaryConversion.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class binaryConversion {// 多进制转换
-	static public void Core(String num, int num1, int num2) {// 核心
+	static public String Core(String num, int num1, int num2) {// 核心
 		System.out.println("转换结果为");
-		String numInput = "Error";
+		String returnNum = "Error";
 		long decimalNumbers = Long.parseLong(num, num1);
 		if (num2 == 2) {
-			numInput = Long.toBinaryString(decimalNumbers);
+			returnNum = Long.toBinaryString(decimalNumbers);
 		} else if (num2 == 8) {
-			numInput = Long.toOctalString(decimalNumbers);
+			returnNum = Long.toOctalString(decimalNumbers);
 		} else if (num2 == 16) {
-			numInput = Long.toHexString(decimalNumbers);
+			returnNum = Long.toHexString(decimalNumbers);
 		} else if (num2 == 10) {
-			numInput = Long.valueOf(decimalNumbers).toString();
+			returnNum = Long.valueOf(decimalNumbers).toString();
 		}
-		System.out.println(numInput);
+		return returnNum;
 	}
 
 	public static void main(String[] args) {
@@ -26,18 +26,22 @@ public class binaryConversion {// 多进制转换
 			String str1 = sc.nextLine();
 			if (InputDetection.intType(str1) == 1) {// 防溢出
 				System.out.println("请输入 2,8,10,16 进制");
+				continue;
 			} else if (Integer.valueOf(str1).intValue() != 2 && Integer.valueOf(str1).intValue() != 8 && Integer.valueOf(str1).intValue() != 10 && Integer.valueOf(str1).intValue() != 16) {
 				System.out.println("请输入 2,8,10,16 进制");
 				continue;
 			}
 			System.out.println("转换为几进制的数");
 			String str2 = sc.nextLine();
-			if (InputDetection.intType(str2) == 1 && Integer.valueOf(str1).intValue() != 2 && Integer.valueOf(str2).intValue() != 8 && Integer.valueOf(str1).intValue() != 10 && Integer.valueOf(str2).intValue() != 16) {// 防溢出
+			if (InputDetection.intType(str2) == 1) {// 防溢出
+				System.out.println("请输入 2,8,10,16 进制");
+				continue;
+			} else if (Integer.valueOf(str2).intValue() != 2 && Integer.valueOf(str2).intValue() != 8 && Integer.valueOf(str2).intValue() != 10 && Integer.valueOf(str2).intValue() != 16) {
 				System.out.println("请输入 2,8,10,16 进制");
 				continue;
 			}
-			sc.close();
-			binaryConversion.Core(str, Integer.valueOf(str1), Integer.valueOf(str2));
+			System.out.println(binaryConversion.Core(str, Integer.valueOf(str1), Integer.valueOf(str2)));
+			break;
 		}
 	}
 }
