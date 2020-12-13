d500e6d911f450f995be27dbefcd6d90892590e0
Your Name <gaogaohahaa@gmail.com>
2020-12-13 21:22:30 +0800

    优化交互

--- binaryConversion.java
+++ binaryConversion.java
@@ -22,23 +22,36 @@ public class binaryConversion {// 多进制转换
 		for (;;) {
 			System.out.println("请输入被转换的数");
 			String str = sc.nextLine();
-			System.out.println("要将什么进制的数进行转换");
-			String str1 = sc.nextLine();
-			if (InputDetection.intType(str1) == 1) {// 防溢出
-				System.out.println("请输入 2,8,10,16 进制");
-				continue;
-			} else if (Integer.valueOf(str1).intValue() != 2 && Integer.valueOf(str1).intValue() != 8 && Integer.valueOf(str1).intValue() != 10 && Integer.valueOf(str1).intValue() != 16) {
-				System.out.println("请输入 2,8,10,16 进制");
-				continue;
+			String str1 = "1";
+			String str2 = "1";
+			for (;;) {
+				System.out.println("要将什么进制的数进行转换");
+				str1 = sc.nextLine();
+				if (InputDetection.intType(str1) == 1) {// 防溢出
+					System.out.println("请输入 2,8,10,16 进制");
+					continue;
+				} else if (Integer.valueOf(str1).intValue() != 2 && Integer.valueOf(str1).intValue() != 8
+						&& Integer.valueOf(str1).intValue() != 10 && Integer.valueOf(str1).intValue() != 16) {
+					System.out.println("请输入 2,8,10,16 进制");
+					continue;
+				} else {
+					break;
+				}
 			}
+
+			for (;;) {
 			System.out.println("转换为几进制的数");
-			String str2 = sc.nextLine();
-			if (InputDetection.intType(str2) == 1) {// 防溢出
-				System.out.println("请输入 2,8,10,16 进制");
-				continue;
-			} else if (Integer.valueOf(str2).intValue() != 2 && Integer.valueOf(str2).intValue() != 8 && Integer.valueOf(str2).intValue() != 10 && Integer.valueOf(str2).intValue() != 16) {
-				System.out.println("请输入 2,8,10,16 进制");
-				continue;
+			str2 = sc.nextLine();
+				if (InputDetection.intType(str2) == 1) {// 防溢出
+					System.out.println("请输入 2,8,10,16 进制");
+					continue;
+				} else if (Integer.valueOf(str2).intValue() != 2 && Integer.valueOf(str2).intValue() != 8
+						&& Integer.valueOf(str2).intValue() != 10 && Integer.valueOf(str2).intValue() != 16) {
+					System.out.println("请输入 2,8,10,16 进制");
+					continue;
+				} else {
+					break;
+				}
 			}
 			System.out.println(binaryConversion.Core(str, Integer.valueOf(str1), Integer.valueOf(str2)));
 			break;
