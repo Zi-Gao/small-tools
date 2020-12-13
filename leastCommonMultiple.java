e90f06d47f9cdea41b72e5c857863ed79e3e9144
Your Name <gaogaohahaa@gmail.com>
2020-12-13 20:27:58 +0800

    分离Core与交互

--- leastCommonMultiple.java
+++ leastCommonMultiple.java
@@ -1,55 +1,55 @@
 import java.util.Scanner;
 
 public class leastCommonMultiple {
-	static public void Core(final String[] arr) {// 核心
-		long c = 1;
-		for (String arrLong : arr) {// 优化,从最大的数开始试,因为最小公倍数不可能小于最大的数
-			if (arrLong == null) {// 已经结束
+	public static long Core(long[] arr) {// 核心
+		long returnValue = 0;
+		for (long tmpStr : arr) {// 优化,从最大的数开始试,因为最小公倍数不可能小于最大的数
+			if (tmpStr == 0) {// 已经结束
 				break;// 停止
-			} else if (Long.valueOf(arrLong).intValue() > c) {// 若有更大的就赋值
-				c = Long.valueOf(arrLong).intValue();// 赋值
+			} else if (Long.valueOf(tmpStr).intValue() > returnValue) {// 若有更大的就赋值
+				returnValue = Long.valueOf(tmpStr).intValue();// 赋值
 			}
 		}
-		Loop: for (; true; c++) {// 暴力搜寻
-			if (c == Long.MAX_VALUE) {// 防溢出
-				System.out.println("此程序记录最小公倍数值为Long,现在为9223372036854775807,继续将会溢出,已自动停止");// 打印
+		loop: for (; true; returnValue++) {// 暴力搜寻
+			if (returnValue == Long.MAX_VALUE) {// 防溢出
 				break;// 停止
 			}
-			for (String bStr : arr) {// 数组测试
+			for (long tmpStr : arr) {// 数组测试
 				// System.out.println(b);
-				if (bStr == null) {// 若为null表示已经结束,输入答案
-					System.out.println("最小公倍数为:" + c);// 打印
-					break Loop;// 停止
-				} else if (c % Long.valueOf(bStr).intValue() != 0) {// 如果不对就试下一个数
-					continue Loop;// 结束本次循环
+				if (tmpStr == 0) {// 若为0表示已经结束,停止
+					break loop;
+				} else if (returnValue % Long.valueOf(tmpStr).intValue() != 0) {// 如果不对就继续
+					continue loop;// 结束本次循环
 				}
 			}
 		}
+		return returnValue;
 	}
 
 	public static void main(String[] args) {// 主方法
 		Scanner sc = new Scanner(System.in);// new控制台输入
 		int i = 0;// 记录输入次数
-		String[] arr = new String[320339960];
-		for (; true;) {// 重复输入
-			if (i == 320339960) {// 防溢出
-				System.out.println("此程序记录因数个最多为320339960,现在为320339960,继续将会溢出,已自动停止");// 输出
+		long[] arr = new long[1024];
+		for (;;) {// 重复输入
+			if (i == 1024) {// 防溢出
+				System.out.println("此程序记录因数个最多为1024,现在为1024,继续将会溢出,已自动停止");// 输出
 				break;// 停止
 			}
 			System.out.println("请输入因数.输入0为开始计算");// 打印标题引导用户输入
-			String str = sc.nextLine();// 输入方法
-			if (InputDetection.longType(str) == 1 | str == null) {// 防溢出,防止直接输入null
+			String tmpStr = sc.nextLine();// 输入方法
+			if (InputDetection.longType(tmpStr) == 1 | tmpStr == null) {// 防溢出,防止直接输入null
 				System.out.println("请输入数字,并为Long类型,请重新输入");// 打印并跳过本次循环
 				continue;
 			}
-			if (Long.valueOf(str).longValue() == 0) {// 调用核心运算
-				leastCommonMultiple.Core(arr);// 调用核心
+			arr[i] = Long.valueOf(tmpStr);// 存入
+			i++;// 自增
+			if (Long.valueOf(tmpStr).longValue() == 0) {// 调用核心运算
+				if(leastCommonMultiple.Core(arr) == 0){
+					System.out.println("最小公倍数达到9223372036854775807,无法继续运算");
+				}
+				System.out.println("最小公倍数为:" + leastCommonMultiple.Core(arr));// 调用核心
 				sc.close();
 				break;
-			}
-			if (Long.valueOf(str).longValue() != 0) {// 自增与存入数组
-				arr[i] = str;// 存入
-				i++;// 自增
 			}
 		}
 	}
